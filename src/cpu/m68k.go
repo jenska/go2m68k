1f3c9d142765de850378769daeed16efb28e6ca1
jensk <jens.kaiser.de@gmail.com>
2017-03-19 16:47:36 +0100

    CPU core functions

--- src/cpu/m68k.go
+++ src/cpu/m68k.go
@@ -1,3 +1,22 @@
 package cpu
 
+import (
+	"log"
+)
 
+
+type M68k struct {
+	 A [8]uint32
+	 D [8]int32
+
+
+	log log.Logger
+}
+
+
+
+
+
+func (cpu *M68k) SetSupervisorMode(mode bool) {
+
+}
---
+++ src/cpu/statusregister.go
@@ -0,0 +1,39 @@
+package cpu
+
+type StatusRegister struct {
+	C, V, Z, N, X, s, T bool
+	Interrupts uint16
+	cpu *M68k
+}
+
+func (sr *StatusRegister) Get() uint16 {
+	result := uint16(0)
+	if sr.C { result += 1 }
+	if sr.V { result += 2 }
+	if sr.Z { result += 4 }
+	if sr.N { result += 8 }
+	if sr.X { result += 16 }
+	if sr.s { result += 0x2000 }
+	if sr.T { result += 0x8000 }
+	result += ((sr.Interrupts & 7) << 8)
+	return result
+}
+
+func (sr *StatusRegister) Set(value uint16) {
+	sr.C = (value & 1) != 0
+	sr.V = (value & 2) != 0
+	sr.Z = (value & 4) != 0
+	sr.N = (value & 8) != 0
+	sr.X = (value & 16) != 0
+	sr.T = (value & 0x8000) != 0
+	sr.Interrupts = (value & 0x0700) >> 8
+	sr.cpu.SetSupervisorMode((value & 0x2000) != 0)
+}
+
+func (sr *StatusRegister) GetCCR() uint16 {
+	return sr.Get() & 0xff
+}
+
+func (sr *StatusRegister) S() bool {
+	return sr.s
+}
