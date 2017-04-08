a0572db09ade085745bb57a32d52846b2b907e57
Jens Kaiser <Jens Kaiser>
2017-04-08 10:36:14 +0200

    fixed instruction model. Added rte, ori ccr/ssr instructions

---
+++ m68k/comtrol.go
@@ -0,0 +1,21 @@
+package m68k
+
+func registerControlInstructions(cpu *M68K) {
+	cpu.registerInstruction(0x0074, ori2ccr)
+	cpu.registerInstruction(0x00f4, ori2sr)
+}
+
+func ori2ccr(cpu *M68K) int {
+	ccr := uint16(cpu.popPC(Word)&0xff) | cpu.SR.GetCCR()
+	cpu.SR.SetCCR(ccr)
+	return 8
+}
+
+func ori2sr(cpu *M68K) int {
+	if cpu.SR.S() {
+		sr := uint16(cpu.popPC(Word)) | cpu.SR.Get()
+		cpu.SR.Set(sr)
+		return 8
+	}
+	return cpu.RaiseException(XptPrv)
+}
