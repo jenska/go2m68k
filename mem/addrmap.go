ef7c5bd620cd5454090d99c9f18a39c6733314ec
jenska <jens.kaiser.de@gmail.com>
2018-10-25 08:59:35 +0000

    refactoring

--- mem/addrmap.go
+++ mem/addrmap.go
@@ -1,4 +1,14 @@
 package mem
 
-type AddressSpace interface {
+type OperandType struct {
+    name string
+    short string
+    mask int
+}
+
+var Word = OperandType("word", ".w", 0xffff)
+
+
+
+type AddressBus interface {
 }
